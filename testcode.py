d10a21aa73c54abafdb74233cf00e7e0d82d8f99
rony728 <rony728@gmail.com>
2017-10-27 18:39:41 -0600

    codeclean

--- testcode.py
+++
@@ -1,5 +0,0 @@
-mas_taxis = int(input(" si el pago de taxis es mas de 40 ingrese 1 de lo contrario ingrese 0: "))
-if mas_taxis == 1 :
-    print(v4+25)
-else:
-    print( "Gracias por usar Calculador")
